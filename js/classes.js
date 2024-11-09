6f41838516f8b5459248a50b1e2f9004a39cf7fb
Dan McCarthy <dmccrthy@users.noreply.github.com>
2024-11-09 18:41:53 -0500

    Add dialog

--- js/classes.js
+++ js/classes.js
@@ -1,86 +1,87 @@
-// // Run on load
-import { getMe } from "./api/user.js";
-
-let classes = [""];
-
-console.log(getMe());
-
-//
-if (classes != [""]) {
-  console.log("hi");
-} else {
-  console.log("no");
-}
+import { getMe, addCourses, removeCourses } from "./api/user.js";
+import { getCourseCRNS } from "./api/course.js";
 
 const body = document.querySelector("body");
+const dialog = document.getElementById("dialog");
 
-////////////////
+async function onPageLoad() {
+    const user = await getMe();
 
-async function addClass(crn) {
-  const res = await fetch("/user/addclass", {
-    method: "POST",
-    headers: {
-      "Content-Type": "text/plain",
-    },
-    body: JSON.stringify([crn]),
-  });
+    if (!user.ok) {
+        alert("NOT LOGGED IN");
+        return;
+    }
 
-  if (res.status !== 200) {
-    return {
-      ok: false,
-      status: res.status,
-    };
-  }
+    addCourses("12527");
+    console.log(user.data.classes);
 
-  return {
-    ok: true,
-  };
+    if (user.data.classes) {
+        let classes = user.data.classes;
+        for (let i = 0; i < classes.length; i++) {
+            console.log(classes[i]);
+        }
+    } else {
+        addCourses("50128");
+    }
+}
+
+function openDialog() {
+    dialog.classList.remove("hidden");
+}
+
+function closeDialog() {
+    dialog.classList.add("hidden");
 }
 
 async function removeClass() {
-  const res = await fetch("/user/removeclass", {
-    method: "POST",
-    headers: {
-      "Content-Type": "text/plain",
-    },
-    body: JSON.stringify([crn]),
-  });
+    const res = await fetch("/user/removeclass", {
+        method: "POST",
+        headers: {
+            "Content-Type": "text/plain",
+        },
+        body: JSON.stringify([crn]),
+    });
 
-  if (res.status !== 200) {
+    if (res.status !== 200) {
+        return {
+            ok: false,
+            status: res.status,
+        };
+    }
+
     return {
-      ok: false,
-      status: res.status,
+        ok: true,
     };
-  }
-
-  return {
-    ok: true,
-  };
 }
 
 /**
  * Handle get user, including there classes
  */
 async function getUser() {
-  const res = await fetch("/user/get", {
-    method: "POST",
-    headers: {
-      "Content-Type": "text/plain",
-    },
-    body: JSON.stringify({
-      username: prompt("Enter username"),
-    }),
-  });
+    const res = await fetch("/user/get", {
+        method: "POST",
+        headers: {
+            "Content-Type": "text/plain",
+        },
+        body: JSON.stringify({
+            username: prompt("Enter username"),
+        }),
+    });
 
-  if (res.status !== 200) {
+    if (res.status !== 200) {
+        return {
+            ok: false,
+            status: res.status,
+        };
+    }
+
     return {
-      ok: false,
-      status: res.status,
+        ok: true,
+        user: await res.json(),
     };
-  }
-
-  return {
-    ok: true,
-    user: await res.json(),
-  };
 }
+
+// Assign function values
+window.onload = onPageLoad;
+window.openDialog = openDialog;
+window.closeDialog = closeDialog;
