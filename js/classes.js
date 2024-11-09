7c5b672516a3dc8a1c3e1a6baab52b2eb41a92bd
z46-dev <evanparker6023@gmail.com>
2024-11-09 13:05:29 -0500

    Dan's add/remove class

--- js/classes.js
+++ js/classes.js
@@ -12,37 +12,71 @@
 
 ////////////////
 
-async function addClass() {
-  return;
+async function addClass(crn) {
+    const res = await fetch("/user/addclass", {
+        method: "POST",
+        headers: {
+            "Content-Type": "text/plain"
+        },
+        body: JSON.stringify([crn])
+    });
+
+    if (res.status !== 200) {
+        return {
+            ok: false,
+            status: res.status
+        };
+    }
+
+    return {
+        ok: true
+    };
 }
 
 async function removeClass() {
-  return;
+    const res = await fetch("/user/removeclass", {
+        method: "POST",
+        headers: {
+            "Content-Type": "text/plain"
+        },
+        body: JSON.stringify([crn])
+    });
+
+    if (res.status !== 200) {
+        return {
+            ok: false,
+            status: res.status
+        };
+    }
+
+    return {
+        ok: true
+    };
 }
 
 /**
  * Handle get user, including there classes
  */
 async function getUser() {
-  const res = await fetch("/user/get", {
-    method: "POST",
-    headers: {
-      "Content-Type": "text/plain",
-    },
-    body: JSON.stringify({
-      username: prompt("Enter username"),
-    }),
-  });
+    const res = await fetch("/user/get", {
+        method: "POST",
+        headers: {
+            "Content-Type": "text/plain",
+        },
+        body: JSON.stringify({
+            username: prompt("Enter username"),
+        }),
+    });
 
-  if (res.status !== 200) {
+    if (res.status !== 200) {
+        return {
+            ok: false,
+            status: res.status,
+        };
+    }
+
     return {
-      ok: false,
-      status: res.status,
+        ok: true,
+        user: await res.json(),
     };
-  }
-
-  return {
-    ok: true,
-    user: await res.json(),
-  };
 }
