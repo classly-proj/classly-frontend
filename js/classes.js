ed1d8e6b6397d906ff6af01225a4384b273e895d
Dan McCarthy <dmccrthy@users.noreply.github.com>
2024-11-09 18:50:43 -0500

    Oops

--- js/classes.js
+++ js/classes.js
@@ -1,6 +1,82 @@
 import { getMe, addCourses, removeCourses } from "./api/user.js";
 import { getCourseCRNS } from "./api/course.js";
 
+async function search() {
+    const res = await fetch("/course/query/list");
+    if (res.status !== 200) {
+        return {
+            ok: false,
+            status: res.status,
+        };
+    }
+    // Internal Key -> Display Name
+    const queriableTypes = await res.json();
+    const box = document.createElement("div");
+    box.style.position = "absolute";
+    box.style.top = "50%";
+    box.style.left = "50%";
+    box.style.transform = "translate(-50%, -50%)";
+    box.style.backgroundColor = "grey";
+    const select = document.createElement("select");
+    select.style.width = "200px";
+    select.style.height = "30px";
+    select.style.fontSize = "20px";
+    for (const [key, value] of Object.entries(queriableTypes)) {
+        const option = document.createElement("option");
+        option.value = key;
+        option.innerText = value;
+        select.appendChild(option);
+    }
+    box.appendChild(select);
+    const input = document.createElement("input");
+    input.type = "text";
+    input.placeholder = "Search";
+    input.style.width = "200px";
+    input.style.height = "30px";
+    input.style.fontSize = "20px";
+    box.appendChild(input);
+    let lastChange = null;
+    let searchKey = null;
+    async function searchTick() {
+        if (input.value === lastChange || input.value.length < 3) {
+            setTimeout(searchTick, 100);
+            return;
+        }
+        searchKey = select.value;
+        lastChange = input.value;
+        let searchValue = lastChange;
+        if (searchKey === "subject-number") {
+            // Grep a string any number of whitespace, and a number
+            const match = lastChange.match(/([a-zA-Z]+)\s*(\d+)/);
+            if (match) {
+                searchValue = `${match[1].toUpperCase()}-${match[2]}`;
+            } else {
+                setTimeout(searchTick, 100);
+                return;
+            }
+        }
+        const response = await fetch("/course/query", {
+            method: "POST",
+            headers: {
+                "Content-Type": "text/plain",
+            },
+            body: JSON.stringify({ key: searchKey, value: searchValue }),
+        });
+        if (response.status !== 200) {
+            return {
+                ok: false,
+                status: response.status,
+            };
+        }
+        const courses = await response.json();
+        window.latestCourses = courses;
+        console.log(courses);
+        setTimeout(searchTick, 100);
+    }
+    searchTick();
+    document.body.appendChild(box);
+}
+
 const body = document.querySelector("body");
 const dialog = document.getElementById("dialog");
 
@@ -27,6 +103,7 @@ async function onPageLoad() {
 
 function openDialog() {
     dialog.classList.remove("hidden");
+    search();
 }
 
 function closeDialog() {
@@ -85,3 +162,4 @@ async function getUser() {
 window.onload = onPageLoad;
 window.openDialog = openDialog;
 window.closeDialog = closeDialog;
+window.search = search;
