9c2ee54df7c035798e6c40096cd590c3da2a1cbd
David Scarborough <dscarb21@gmail.com>
2024-11-09 16:24:08 -0500

    live nav in prog

--- js/mapbox.js
+++ js/mapbox.js
@@ -1,152 +1,206 @@
 import { loadDirections } from "./api/mapbox.js";
 
 const bounds = [
-  [-70.956425, 43.118725], 
-  [-70.894926, 43.154229],
+    [-70.956425, 43.118725],
+    [-70.894926, 43.154229],
 ];
 
-mapboxgl.accessToken = "pk.eyJ1IjoiZHNjYXJiMjEiLCJhIjoiY2x0cnR3cWlqMGtmZzJucDU2eDR2eWpyMCJ9.nfk8bnbhwkUmEHDhKZv3zA";
-
+mapboxgl.accessToken =
+    "pk.eyJ1IjoiZHNjYXJiMjEiLCJhIjoiY2x0cnR3cWlqMGtmZzJucDU2eDR2eWpyMCJ9.nfk8bnbhwkUmEHDhKZv3zA";
 
 const map = new mapboxgl.Map({
-  container: "mapbox", 
-  style: "mapbox://styles/mapbox/streets-v12",
-  zoom: 12, 
-  center: [-70.92560, 43.13401],
-  maxBounds: bounds, 
+    container: "mapbox",
+    style: "mapbox://styles/mapbox/streets-v12",
+    zoom: 12,
+    center: [-70.9256, 43.13401],
+    maxBounds: bounds,
 });
 
-const start = [-70.930745,43.152942];
+let watchId = null;
+let currCoords = null;
 
-async function loadRoute(end) {
-  const response = await loadDirections(start[0], start[1], end[0], end[1]);
-
-  if (!response.ok) {
-    console.error('Error loading directions:', response.getStatusName());
-    return;
-  }
-
-  const json = response.data;
-  const data = json.routes[0];
-  const route = data.geometry.coordinates;
-  const geojson = {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'LineString',
-      coordinates: route
-    }
-  };
-
-  if (map.getSource('route')) {
-    map.getSource('route').setData(geojson);
-  } else {
+function initLocationLayer(coords) {
     map.addLayer({
-      id: 'route',
-      type: 'line',
-      source: {
-        type: 'geojson',
-        data: geojson
-      },
-      layout: {
-        'line-join': 'round',
-        'line-cap': 'round'
-      },
-      paint: {
-        'line-color': '#3887be',
-        'line-width': 5,
-        'line-opacity': 0.75
-      }
+        id: "location",
+        type: "circle",
+        source: {
+            type: "geojson",
+            data: {
+                type: "FeatureCollection",
+                features: [
+                    {
+                        type: "Feature",
+                        properties: {},
+                        geometry: {
+                            type: "Point",
+                            coordinates: coords,
+                        },
+                    },
+                ],
+            },
+        },
+        paint: {
+            "circle-radius": 10,
+            "circle-color": "#3887be",
+        },
     });
-  }
 }
 
-function loadBuildings(buildings) {
-  const allFeatures = [];
-  for (const floor in buildings.paul) {
-    if (buildings.paul.hasOwnProperty(floor)) {
-      buildings.paul[floor].forEach((entry) => {
-        allFeatures.push({
-          type: 'Feature',
-          properties: {
-            floor: floor
-          },
-          geometry: {
-            type: 'Point',
-            coordinates: entry.coordinates
-          }
+function updateLocation(coords) {
+    console.log("UPDATE");
+    if (map.getSource("location")) {
+        map.getSource("location").setData({
+            type: "FeatureCollection",
+            features: [
+                {
+                    type: "Feature",
+                    properties: {},
+                    geometry: {
+                        type: "Point",
+                        coordinates: coords,
+                    },
+                },
+            ],
         });
-      });
     }
-  }
-
-  // Combine all features into a single FeatureCollection
-  const combinedData = {
-    type: 'FeatureCollection',
-    features: allFeatures
-  };
-
-  // Add the combined GeoJSON source to the map
-  map.addSource('combined_buildings', {
-    type: 'geojson',
-    data: combinedData
-  });
-
-  // Add the layer using the combined source
-  map.addLayer({
-    id: 'buildings',
-    type: 'circle',
-    source: 'combined_buildings',
-    paint: {
-      'circle-radius': 8,
-      'circle-color': '#ff8c00'
-    }
-  });
-
-  map.on('click', 'buildings', function (e) {
-    const coordinates = e.features[0].geometry.coordinates;
-    loadRoute(coordinates);
-  });
-
-  map.on('mouseenter', 'buildings', function () {
-    map.getCanvas().style.cursor = 'pointer';
-  });
-
-  map.on('mouseleave', 'buildings', function () {
-    map.getCanvas().style.cursor = '';
-  });
 }
 
-map.on('load', () => {
-  map.addLayer({
-    id: 'point',
-    type: 'circle',
-    source: {
-      type: 'geojson',
-      data: {
-        type: 'FeatureCollection',
-        features: [
-          {
-            type: 'Feature',
-            properties: {},
-            geometry: {
-              type: 'Point',
-              coordinates: start
+function startWatchingLocation() {
+    if ("geolocation" in navigator) {
+        watchId = navigator.geolocation.watchPosition(
+            (position) => {
+                const { latitude, longitude } = position.coords;
+                currCoords = [longitude, latitude];
+                if (map.getLayer("location")) {
+                    updateLocation(currCoords);
+                } else {
+                    initLocationLayer(currCoords);
+                }
+            },
+            (error) => {
+                console.error("Geolocation error: ", error);
+            },
+            {
+                enableHighAccuracy: true,
+                timeout: 5000,
+                maximumAge: 5000,
             }
-          }
-        ]
-      }
-    },
-    paint: {
-      'circle-radius': 10,
-      'circle-color': '#3887be'
+        );
+    } else {
+        console.error("Geolocation is not supported by this browser.");
     }
-  });
+}
 
-  fetch('./data/buildings.json') 
-      .then(response => response.json())
-      .then(buildings => {
-        loadBuildings(buildings);
-      })
-      .catch(error => console.error('Error loading the JSON file:', error));
+function stopWatchingLocation() {
+    if (watchId !== null) {
+        navigator.geolocation.clearWatch(watchId);
+        watchId = null;
+    }
+}
+
+async function loadRoute(start, end) {
+    const targetUrl = `https://api.mapbox.com/directions/v5/mapbox/walking/${start[0]}%2C${start[1]}%3B${end[0]}%2C${end[1]}?alternatives=true&geometries=geojson&language=en&overview=full&steps=true&access_token=pk.eyJ1IjoiZHNjYXJiMjEiLCJhIjoiY2x0cnR1Y254MGkxdzJqbnZ2bHowcHN1MiJ9.TSvhTRQ2X-G0M-mzagneOw`;
+
+    if (!response.ok) {
+        console.error("Error loading directions:", response.getStatusName());
+        return;
+    }
+
+    const json = response.data;
+    const data = json.routes[0];
+    const route = data.geometry.coordinates;
+    const geojson = {
+        type: "Feature",
+        properties: {},
+        geometry: {
+            type: "LineString",
+            coordinates: route,
+        },
+    };
+
+    if (map.getSource("route")) {
+        map.getSource("route").setData(geojson);
+    } else {
+        map.addLayer({
+            id: "route",
+            type: "line",
+            source: {
+                type: "geojson",
+                data: geojson,
+            },
+            layout: {
+                "line-join": "round",
+                "line-cap": "round",
+            },
+            paint: {
+                "line-color": "#3887be",
+                "line-width": 5,
+                "line-opacity": 0.75,
+            },
+        });
+    }
+}
+
+async function loadBuildings(buildings) {
+    try {
+        const response = await fetch("./data/buildings.json");
+        const buildings = await response.json();
+        const allFeatures = [];
+        for (const floor in buildings.paul) {
+            if (buildings.paul.hasOwnProperty(floor)) {
+                buildings.paul[floor].forEach((entry) => {
+                    allFeatures.push({
+                        type: "Feature",
+                        properties: {
+                            floor: floor,
+                        },
+                        geometry: {
+                            type: "Point",
+                            coordinates: entry.coordinates,
+                        },
+                    });
+                });
+            }
+        }
+
+        const combinedData = {
+            type: "FeatureCollection",
+            features: allFeatures,
+        };
+
+        map.addSource("combined_buildings", {
+            type: "geojson",
+            data: combinedData,
+        });
+
+        map.addLayer({
+            id: "buildings",
+            type: "circle",
+            source: "combined_buildings",
+            paint: {
+                "circle-radius": 8,
+                "circle-color": "#ff8c00",
+            },
+        });
+
+        map.on("click", "buildings", async function (e) {
+            const endCoords = e.features[0].geometry.coordinates;
+            loadRoute(currCoords, endCoords);
+        });
+
+        map.on("mouseenter", "buildings", function () {
+            map.getCanvas().style.cursor = "pointer";
+        });
+
+        map.on("mouseleave", "buildings", function () {
+            map.getCanvas().style.cursor = "";
+        });
+    } catch (error) {
+        console.error("Error loading buildings:", error);
+    }
+}
+
+map.on("load", async function () {
+    startWatchingLocation();
+    loadBuildings();
 });
